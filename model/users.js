b0847644d90c8e5a5a364f4f84aeee7a451dfe3a
Sajid Ansari <sajidansari33272@gmail.com>
2020-06-01 17:43:47 +0545

    Custom error validation is added in joi

--- model/users.js
+++ model/users.js
@@ -86,14 +86,6 @@ function validateUser(user) {
   });
 
   return schema.validate(user);
-
-  // /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,255}$/
-  //     /^
-  //   (?=.*\d)          // should contain at least one digit
-  //   (?=.*[a-z])       // should contain at least one lower case
-  //   (?=.*[A-Z])       // should contain at least one upper case
-  //   [a-zA-Z0-9]{8,}   // should contain at least 8 from the mentioned characters
-  // $/
 }
 
 const User = mongoose.model("User", UserSchema);
