67fa33cd254d5982fc440e89679732b8efd37bbb
Sajid Ansari <sajidansari33272@gmail.com>
2020-06-01 17:39:35 +0545

    Custum error message is added in joi

--- model/users.js
+++ model/users.js
@@ -55,16 +55,34 @@ UserSchema.pre("remove", async function (next) {
 
 function validateUser(user) {
   const schema = Joi.object({
-    username: Joi.string().min(3).max(50).required(),
+    username: Joi.string().empty().min(3).max(50).required().messages({
+      "string.base": `username should be a type of 'text'`,
+      "string.min": `username should have a minimum length of {3}`,
+      "string.max": `username should have a maximum length of {50}`,
+      "string.empty": `username cannot be an empty feild`,
+      "any.required": `username is a required field`,
+    }),
     email: Joi.string()
       .required()
-      .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } }),
-    passwordHash: Joi.string().pattern(
-      new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})")
-    ),
+      .empty()
+      .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+      .messages({
+        "any.required": `Email is a required field `,
+        "string.empty": `Email cannot be an empty feild`,
+      }),
+    passwordHash: Joi.string()
+      .pattern(
+        new RegExp(
+          "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"
+        )
+      )
+      .message(
+        "Password sholud be at lest one digit, one lower and upper case ,one spcial charcter like @%!&^*, and must be 8 character long"
+      ),
+
     // for password confirmation
 
-    // password_confirmation: Joi.any().valid(Joi.ref('password')).required().options({ language: { any: { allowOnly: 'must match password' } } })
+    //Joi.ref('passwordHash')
   });
 
   return schema.validate(user);
